5e94431050f7540c2850e9c94a54d3197e4ec057
y.pashkovskiy <y.pashovkiy@mail.ru>
2026-01-20 13:21:31 +0400

    Задача №14 Второй вариант

---
+++ Task14_2.php
@@ -0,0 +1,50 @@
+<?php
+   /*Есть два массива "a" и "b". На вход вашей программе подаётся массив "a"
+    случайных чисел (10 элементов) из диапазона от 1 до 20.
+     Задача: вывести на экран все числа, которые не содержатся в массиве "b".
+     С использованием in_array()*/
+
+   function fillingArray($count)
+   {
+             for ($loop_index = 0; $loop_index < $count; $loop_index++) {
+                    $arr[$loop_index] = rand(1, 20);
+                }
+                 
+                return $arr;
+   } 
+
+
+
+   function comparisonArray ($arr1, $arr2)
+            {   
+             
+             $index = 0; 
+             foreach ($arr1 as $outer_key => $value){  
+              
+                if (!(in_array($value, $arr2))) {
+                   $arr[$index] = $value;
+                   $index++;
+             }
+
+                
+        }
+
+               
+                 
+                return $arr;
+            }
+
+
+            $n = 10;
+            $array1 = fillingArray($n);
+            $array2 = fillingArray($n);
+
+           $сomparativeArray = comparisonArray ($array1, $array2);
+
+            echo "Вывод на экран все числа, которые есть в массиве \"a\" и не содержатся в массиве \"b\" \n";
+          
+            foreach ($сomparativeArray as $value) {
+                echo "$value  \n";
+            }
+          
+?>
